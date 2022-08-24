19bdd49a915ad85ccfc087748aca0c78725034df
Atalaia <maria.soage01@gmail.com>
2022-08-24 15:04:58 +0200

    Adding a Background Image, logic to the buttons in StartGameScreen and Showing an Alert Dialog

--- guess-the-number-mini-game/App.js
+++ guess-the-number-mini-game/App.js
@@ -1,11 +1,18 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 
 const App = () => {
   return (
     <LinearGradient style={styles.rootScreen} colors={["#4e0329", "#ddb52f"]}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 };
@@ -15,6 +22,8 @@ export default App;
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#ddb52f",
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
