34f0628acadafaf578ea7f49a78d59e1d7541ae3
Atalaia <maria.soage01@gmail.com>
2022-08-24 10:54:50 +0200

    first commit mini game app

---
+++ guess-the-number-mini-game/App.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
+
+const App = () => {
+  return <StartGameScreen />;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
