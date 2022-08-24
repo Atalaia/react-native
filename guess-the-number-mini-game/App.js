1e200f79f21d80a88b6894b010ac7c40ce37252b
Atalaia <maria.soage01@gmail.com>
2022-08-24 14:15:02 +0200

    Adding Linear Gradient to the app background

--- guess-the-number-mini-game/App.js
+++ guess-the-number-mini-game/App.js
@@ -1,10 +1,20 @@
 import { StyleSheet } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 const App = () => {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient style={styles.rootScreen} colors={["#4e0329", "#ddb52f"]}>
+      <StartGameScreen />
+    </LinearGradient>
+  );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: "#ddb52f",
+  },
+});
