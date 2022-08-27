a7dd64532da46ca7b2e2497f4115ace6aed5275b
Atalaia <maria.soage01@gmail.com>
2022-08-27 14:36:42 +0200

    [EXPENSES TRACKER APP] First commit

---
+++ expense-tracker-app/screens/RecentExpenses.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function RecentExpenses() {
+  return (
+    <View>
+      <Text>Recent Expenses Screen</Text>
+    </View>
+  );
+}
+
+export default RecentExpenses;
+
+const styles = StyleSheet.create({});
