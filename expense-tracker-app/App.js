431a9b06438cc3cf93be671ba1372f8159db0008
Atalaia <maria.soage01@gmail.com>
2022-08-28 00:15:02 +0200

    [EXPENSES TRACKER APP] Adding a header button and making items tappable, navigating programmatically between scenes, styling the expense management screen and supporting different editing modes and using route parameters

--- expense-tracker-app/App.js
+++ expense-tracker-app/App.js
@@ -7,6 +7,7 @@ import { Ionicons } from "@expo/vector-icons";
 import ManageExpense from "./screens/ManageExpense";
 import RecentExpenses from "./screens/RecentExpenses";
 import AllExpenses from "./screens/AllExpenses";
+import IconButton from "./components/ui/IconButton";
 
 import { GlobalStyles } from "./utils/styles";
 
@@ -16,12 +17,22 @@ const BottomTabs = createBottomTabNavigator();
 function ExpensesOverview() {
   return (
     <BottomTabs.Navigator
-      screenOptions={{
+      screenOptions={({ navigation }) => ({
         headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         headerTintColor: "white",
         tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         tabBarActiveTintColor: GlobalStyles.colors.accent500,
-      }}
+        headerRight: ({ tintColor }) => (
+          <IconButton
+            icon="add"
+            size={24}
+            color={tintColor}
+            onPress={() => {
+              navigation.navigate("ManageExpense");
+            }}
+          />
+        ),
+      })}
     >
       <BottomTabs.Screen
         name="RecentExpenses"
@@ -54,7 +65,12 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            headerTintColor: "white",
+          }}
+        >
           <Stack.Screen
             name="ExpensesOverview"
             component={ExpensesOverview}
@@ -62,7 +78,11 @@ export default function App() {
               headerShown: false,
             }}
           />
-          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+          <Stack.Screen
+            name="ManageExpense"
+            component={ManageExpense}
+            options={{ presentation: "modal" }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- expense-tracker-app/screens/ManageExpense.js
+++ expense-tracker-app/screens/ManageExpense.js
@@ -1,6 +1,19 @@
+import { useLayoutEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+
+  // To convert editedExpenseId into a boolean
+  // True if we are editing, false otherwise
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return (
     <View>
       <Text>Manage Expense Screen</Text>
